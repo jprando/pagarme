3260b904504675595f3ede08141e1eefae106f97
Jeudi Prando <jeudi@adsoft.com.br>
2020-02-04 12:37:31 -0300

    user findall attributes with filter

--- services/user/findAll.js
+++ services/user/findAll.js
@@ -1,16 +1,6 @@
 module.exports = async function () {
   const { db: { user } } = this
   return user.findAll({
-    attributes: [
-      'id',
-      'name',
-      'email',
-      'active',
-      'ukey',
-      'lastLoginAt',
-      'lastPasswordChangeAt',
-      'createdAt',
-      'updatedAt'
-    ]
+    attributes: Object.keys(user.tableAttributes).filter(f => f !== 'credential')
   })
 }
