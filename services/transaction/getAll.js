3fc84befb51bc377d3bfc1c25a1751ca7061dfd4
Jeudi Prando <jeudi@adsoft.com.br>
2020-02-06 17:49:32 -0300

    messages of statusCode only controller

--- services/transaction/getAll.js
+++ services/transaction/getAll.js
@@ -1,5 +1,4 @@
 module.exports = async function () {
   const { db: { transaction, toPlain } } = this
-  return transaction.findAll().map(toPlain) ||
-  { error: true, code: 404, message: 'No Transactions' }
+  return transaction.findAll().map(toPlain)
 }
