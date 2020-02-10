83d384a55f21000f3a356529ff1e5ea7ed68d34d
Jeudi Prando <jeudiprando@gmail.com>
2020-02-10 01:26:37 -0300

    validate customer balance params

--- controllers/customer/getCustomerBalance.js
+++ controllers/customer/getCustomerBalance.js
@@ -5,7 +5,26 @@ module.exports = dataResponse(async ({
   services: { customer },
   params
 }) => {
-  const year = Number.parseInt(params.year) || new Date().getUTCFullYear()
-  const month = Number.parseInt(params.month) || new Date().getUTCMonth() + 1
+  const yearNow = new Date().getUTCFullYear()
+  const monthNow = new Date().getUTCMonth() + 1
+  let year = Number.parseInt(params.year)
+  let month = Number.parseInt(params.month)
+
+  if (year < 1990 || year > yearNow) {
+    return { error: true, code: 400, message: `year must be between 1990 and ${yearNow}` }
+  }
+  if (month < 1) {
+    return { error: true, code: 400, message: 'month must be a positive number, greater than zero' }
+  }
+  if (year === yearNow && month > monthNow) {
+    return { error: true, code: 400, message: 'month not be in the future' }
+  }
+  if (year !== yearNow && month > 12) {
+    return { error: true, code: 400, message: 'month must be between 1 and 12' }
+  }
+
+  year = year || yearNow
+  month = month || monthNow
+
   return customer.balance(ukey, year, month)
 })
